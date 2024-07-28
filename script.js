6bfc5305cd425dc4bc08d12f483e443b5c4552f7
snehashuklaa <shsneha436@gmail.com>
2024-07-28 19:13:07 +0530

    New Code commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// const swiper = new Swiper('.slider-wrapper', {
+//     loop: true,
+//     grabCursor: true,
+//     spaceBetween: 30,
+  
+//     // If we need pagination
+//     pagination: {
+//       el: '.swiper-pagination',
+//       clickable: true,
+//       dynamicBullets: true,
+//     },
+  
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+//     //Responsive breakpoints
+//   breakpoints: {
+//     0: {
+//         slidesPerView: 1
+//     },
+//     768: {
+//         slidesPerView: 1
+//     },
+//     1024: {
+//         slidesPerView: 1
+//     },
+//   }
+
+//   });
+  
